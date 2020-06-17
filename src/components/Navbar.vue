9664e0b7d00d1990487af499c0ca0ca18a30473f
Ginakira <ginakira@foxmail.com>
2020-06-17 23:57:09 +0800

    feat: 导航栏组件

---
+++ src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        transition="scale-transition"
+        width="40"
+      />
+
+      <v-img
+        alt="Vuetify Name"
+        class="shrink mt-1 hidden-sm-and-down"
+        contain
+        min-width="100"
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+        width="100"
+      />
+    </div>
+    <v-tabs>
+      <v-tab to="/">Home</v-tab>
+      <v-tab>Faculty</v-tab>
+      <v-tab to="/about">About</v-tab>
+    </v-tabs>
+    <v-spacer></v-spacer>
+
+    <v-btn href="http://47.92.91.148" target="_blank" text>
+      <span class="mr-2">OJ平台</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data: () => ({})
+};
+</script>
